feecb6fb9feb383ec23c81db6f1b46619b4bcf7e
nive-dev <nivethithakesavan7823@gmail.com>
2026-01-03 11:18:48 +0530

    git commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  base: "/kousthubam-website-dynamic-link/",
+  base: "/Thiruannamalaiyar-website-dynamic-link/",
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
