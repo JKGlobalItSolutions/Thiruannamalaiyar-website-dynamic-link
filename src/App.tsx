1258b24251b0990074e8557b30e98abef2c56950
nive-dev <nivethithakesavan7823@gmail.com>
2026-01-22 18:03:14 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename="/Thiruannamalaiyar-website-dynamic-link/">
+        <BrowserRouter basename="/Dss-website-dynamic-link/">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const config = {
-  HOTEL_ID: '6957a76aa843b26b156d35cd',
+  HOTEL_ID: '69720c347ee2b2e712b7a8f3',
   API_BASE: 'https://sonachala-single-booking-backend.onrender.com',
-  UPI_ID: '8438438413@okbizaxis',
+  UPI_ID: 'duraisamy20051963@okhdfcbank',
   GOOGLE_MAPS_EMBED_URL: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.7765499999997!2d79.070466!3d12.280244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDE2JzQ4LjgiTiA3OcKwMDQnMTYuNyJF!5e0!3m2!1sen!2sin!4v1635000000000!5m2!1sen!2sin'
 } as const;
